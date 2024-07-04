e063e6aeaf7f75958e86a5e3f17843c33f4c7adf
宮田光 <7k00xrisa@gmail.com>
2024-07-04 16:33:59 +0900

    コミット

--- flea_market_app/lib/screens/register_screen.dart
+++ flea_market_app/lib/screens/register_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:convert'; // Base64エンコード用
-import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../db_helper.dart';
 import '../models/user.dart';
